88f2ddbd45b1075cae35759d1f364b143133e5da
Sudhil Gauchan <sudhilgauchan@MacBook-Pro.local>
2023-04-13 10:56:09 -0500

    Initial Commit

---
+++ src/Day1/Address.java
@@ -0,0 +1,12 @@
+package Day1;
+
+public class Address {
+
+    /* States */
+    private Long addressId;
+    private String streetAddress;
+    private String city;
+    private String state;
+    private String zipCode;
+
+}
---
+++ src/Day1/Person.java
@@ -0,0 +1,16 @@
+package Day1;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Person {
+
+    /* States */
+    private Long person_Id;
+    private String name;
+    private int age;
+    private String gender;
+    private BigDecimal salary;
+    private Boolean isStudent;
+    private LocalDateTime birthday;
+}
