2c339fa18f53a49a5a5cb1b612f64c5b3eae7175
Sudhil Gauchan <sudhilgauchan@MacBook-Pro.local>
2023-05-10 18:17:16 -0500

    Immutable Class Assignment

---
+++ src/immutableAssignment/Student.java
@@ -0,0 +1,33 @@
+package immutableAssignment;
+
+
+/**
+ *
+ *  Immutable class is a class whose content cannot be changed once object created. For eg: String class, DateTimeFormatter class, etc.
+ *  Custom immutable class can be created by:
+ *  1. Making class final so that it cannot be extended by any other class.
+ *  2. Making data members private and final so that data member's value cannot be changed after object is created.
+ *  3. Class should have only getter method but no any setter method.
+ *
+ */
+public final class Student {
+
+    // private and final data member
+    private final String name;
+
+    // Parameterized constructor
+    public Student(String name) {
+        this.name = name;
+    }
+
+    // Getter Method
+    public String getName() {
+        return name;
+    }
+
+    public static void main(String[] args) {
+        Student student = new Student("John");
+        String studentName = student.getName();
+        System.out.println(studentName);
+    }
+}
