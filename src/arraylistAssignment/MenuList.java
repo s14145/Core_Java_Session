909347dc171ee9f1a1c244326e6e6cbed6536a14
Sudhil Gauchan <sudhilgauchan@MacBook-Pro.local>
2023-06-19 21:39:20 -0500

    ArrayList Update Delete Assignment

--- src/arraylistAssignment/MenuList.java
+++ src/arraylistAssignment/MenuList.java
@@ -1,12 +1,15 @@
 package arraylistAssignment;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class MenuList {
 
     public static void main(String[] args) {
 
-        ArrayList<Menu> menuList = new ArrayList<>();
+        List<Menu> menuList = new ArrayList<>();
+
         menuList.add(new Menu(1, "Women Fashion", 0, false, "women-fashion"));
         menuList.add(new Menu(2, "Men Fashion", 0, false, "men-fashion"));
         menuList.add(new Menu(3, "Clothing", 1, false, "women-fashion"));
@@ -14,7 +17,7 @@ public class MenuList {
         menuList.add(new Menu(5, "Saree", 3, true, "women-clothing-saree"));
         menuList.add(new Menu(6, "T-Shirts", 4, true, "men-clothing-t-shirts"));
 
-        // Printing all the menu in the menuList
+        System.out.println("Printing all the menu in the menuList using Enhanced For Loop: ");
         for (Menu menu : menuList) {
             System.out.println(menu);
         }
@@ -23,9 +26,10 @@ public class MenuList {
         menuList.set(4, new Menu(5, "Skirts", 3, true, "women-clothing-skirts"));
 
         System.out.println("\n");
-        System.out.println("Printing all the menu after update operation:");
-        for (Menu menu : menuList) {
-            System.out.println(menu);
+        System.out.println("Printing all the menu after update operation using Iterator:");
+        Iterator<Menu> menu = menuList.iterator();
+        while(menu.hasNext()){
+            System.out.println(menu.next());
         }
 
         // Delete menu from menuList
@@ -33,9 +37,7 @@ public class MenuList {
         menuList.remove(4);
 
         System.out.println("\n");
-        System.out.println("Printing all the menu after delete operation:");
-        for (Menu menu : menuList) {
-            System.out.println(menu);
-        }
+        System.out.println("Printing all the menu after delete operation using Stream API forEach method:");
+        menuList.forEach(System.out::println);
     }
 }
